bf5e1908a9b0c78d87f10078948c59bdaa92fede
itsamanverma28 <aman@livwell.asia>
2024-05-01 16:47:05 +0530

    LMS project

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import bookRoutes from "./routes/books.js";
+import transactionRoutes from "./routes/transactions.js";
+import categoryRoutes from "./routes/categories.js";
+
+/* App Config */
+dotenv.config();
+const app = express();
+const port = 5000;
+
+/* Middlewares */
+app.use(express.json());
+app.use(cors());
+
+/* API Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/categories", categoryRoutes);
+
+/* MongoDB connection */
+mongoose.connect(
+  "mongodb+srv://amanvermame786:amanvermame786@cluster0.5xogbei.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("MONGODB CONNECTED");
+  }
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to LibraryApp");
+});
+
+/* Port Listening In */
+app.listen(port, () => {
+  console.log(`Server is running in PORT ${port}`);
+});
